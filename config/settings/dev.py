ca6a23143ef541c708a4fb08af6cb17adfb53a40
C1904H <164913030+C1904H@users.noreply.github.com>
2025-01-19 12:55:48 +0000

    initial commit

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -17,22 +17,22 @@ THIRD_PARTY_APP = [
 INSTALLED_APPS = INSTALLED_APPS + THIRD_PARTY_APP
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env.str("DB_NAME"),
-        "USER": env.str("DB_USER"),
-        "PASSWORD": env.str("DB_PWD"),
-        "HOST": env.str("DB_HOST"),
-        "PORT": env.str("DB_PORT"),
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": env.str("DB_NAME"),
+#         "USER": env.str("DB_USER"),
+#         "PASSWORD": env.str("DB_PWD"),
+#         "HOST": env.str("DB_HOST"),
+#         "PORT": env.str("DB_PORT"),
+#     }
+# }
 
 # If you want to use sqlite3 instead, then uncomment this and comment the above.
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
